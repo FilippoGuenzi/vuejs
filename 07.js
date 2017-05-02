eec1cc4e306329f91b1058e691456a37c16af3a0
Filippo Guenzi <filippoguenzi@hotmail.com>
2017-05-02 07:40:32 +0200

    temp

---
+++ 07.js
@@ -0,0 +1,10 @@
+new Vue({
+    el: "#app",
+    data: {
+        tasks: [
+            {body: 'Go to the bank', completed: false},
+            {body: 'Go to the doctor', completed: false},
+            {body: 'Go to the grocery', completed: true}
+        ]
+    }
+});
