0fba6a5a4048d2015e018801376587ff251f15f4
Filippo Guenzi <filippoguenzi@hotmail.com>
2017-05-26 16:10:44 +0200

    tested vue-resource

--- exercices/11/11.js
+++ exercices/11/11.js
@@ -9,6 +9,8 @@ Vue.component('tasks', {
     ,
     created: function () {
         this.fetchTasks();
+        this.resource = this.$resource('http://localhost:8080/tasks-server/task{/id}');
+        this.getTaskById();
     }
     ,
     methods: {
@@ -19,6 +21,12 @@ Vue.component('tasks', {
                 response => {that.list = response.body;},
                 response => {console.log(JSON.stringify(response));}
             );
+        },
+
+        getTaskById: function(){
+            this.resource.get({id:3}).then(response => {
+                console.log(response.body);
+            });
         },
 
         deleteTask: function (task) {
